d604ba918fa1bfa21f12870fdeb16e242b2b013e
Cowboy Lynk <lynk.cowboy@gmail.com>
2018-07-19 21:23:53 -0400

    missed some files because I am trying to stop tracking the .idea folder

--- courses_database/static/js/scheduler-init.js
+++ courses_database/static/js/scheduler-init.js
@@ -1,3 +1,22 @@
+function calendarInit(){
+    $('#calendar').fullCalendar({
+        defaultDate: moment('2018-01-01'),
+        weekends: false,
+        defaultView: 'agendaWeek',
+        columnHeaderFormat: 'dddd',
+        minTime: "08:00:00",
+        maxTime: "23:59:00",
+        height: "auto",
+        eventColor: '#29B89B',
+        header:false,
+
+        contentHeight: 600,
+        allDaySlot: false,
+
+        events: [{}]
+    });
+}
+
 function tablesInit(){
     $('#add_courses_table').DataTable();
 }
