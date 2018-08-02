2e8d95487d984372095a8fcec72d7868d16190c6
Cowboy Lynk <lynk.cowboy@gmail.com>
2018-08-02 18:27:53 -0400

    commented views.py

--- courses_database/views.py
+++ courses_database/views.py
@@ -8,19 +8,33 @@ import json
 
 
 def index(request):
+	"""
+	The home screen of the app that displays the calendar and lets users add courses and sort through schedules
+	:return:
+			sections (array): an array of courses used to populate the data table
+			courses_info (dictionary): a dictionary that maps "subject + course_id" to descriptions
+	"""
 	unique_sections = set()
 	sections = []
+	courses_info = {}
 	for section in Section.objects.all():
 		subject, course_id = section.subject, section.course_id
 		# makes sure that we don't add multiple sections for the same class
 		if subject + course_id not in unique_sections:
 			sections.append(section)
 			unique_sections.add(subject + course_id)
-			
-	return render(request, 'index.html', {"sections": sections})
+			courses_info[subject + course_id] = section.description
+	
+	return render(request, 'index.html', {"sections": sections, "courses_info": json.dumps(courses_info)})
 
 
 def get_schedules(request):
+	"""
+	Called by AJAX in background to populate the calendar full of schedules
+	:return:
+			schedules (dict): this structure is super complex (see Slack), but basically it contains the schedules
+			schedules_info (dict): maps "subject + course_id" to string descriptions of that course
+	"""
 	if request.POST:
 		# gets the relevant schedule request data from the post request
 		schedule_restrictions = json.loads(request.POST["schedule_restrictions"])
@@ -37,6 +51,12 @@ def get_schedules(request):
 	
 	
 def get_sections(request):
+	"""
+	(NOT CURRENTLY BEING USED)
+	Used to populate the data tables when using AJAX instead of Django template tags
+	:return: sections_list (json array): array of dicts representing sections
+	"""
+	# TODO: IF you use this function, make sure to only add unique courses, not every section
 	sections_list = Section.objects.all()
 	json_sections = serializers.serialize("json", sections_list)
 	return HttpResponse(json_sections, content_type='application/json')
